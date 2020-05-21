64875aec42bd3c2112fca4bd17a72988c37a4973
Renzo Gabriel Bongocan <renzo@kalibrr.com>
2020-05-21 19:53:23 +0800

    Limit messaging attempts to 2 per listing; upon failure move on to next listing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 from requests.exceptions import RequestException, Timeout
 from pprint import PrettyPrinter
+from telegram.error import TelegramError, TimedOut, NetworkError
 from typing import Any, Dict, List
 
+import logging
 import re
 import requests
 import telegram
@@ -21,18 +23,13 @@ BOT = telegram.Bot(config["telegram"]["bot"]["token"])
 CHANNEL = config["telegram"]["channel"]
 
 
-LISTING_REGEXP = "^/p/"
-LISTING_CURRENCY = "PHP"
-LISTING_SIZE_PREFIX = "Size:"
-
-
 def get_listings(brand: str) -> List[Any]: 
     url = CAROUSELL_URL.format(search=brand.replace("20%"," "))
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text, "html.parser")
 
     listings: List[CarousellListing] = [] 
-    for listing_node in soup.find_all("a", href=re.compile(LISTING_REGEXP)):
+    for listing_node in soup.find_all("a", href=re.compile("^/p/")):
         listing = CarousellListing(url=f"{CAROUSELL_HOST}{listing_node.get('href')}")
         for p_idx, p in enumerate(listing_node.find_all("p", recursive=False)):
             if not (listing_info := p.string):
@@ -41,9 +38,9 @@ def get_listings(brand: str) -> List[Any]:
             key = "description"
             if p_idx == 0:
                 key = "title"
-            elif listing_info.startswith(LISTING_CURRENCY):
+            elif listing_info.startswith("PHP "):
                 key = "price"
-            elif listing_info.startswith(LISTING_SIZE_PREFIX):
+            elif listing_info.startswith("Size: "):
                 key = "size"
             
             if key == "description":
@@ -57,7 +54,7 @@ def get_listings(brand: str) -> List[Any]:
     
 
 if __name__ == "__main__":
-    for brand in {"APC", "Commes des Garcons", "Nanamica"}:
+    for brand in ("APC", "Commes des Garcons", "Nanamica"):
         listings = get_listings(brand)
         for idx, listing in enumerate(listings):
             # rate-limit for dev purposes
@@ -65,17 +62,28 @@ if __name__ == "__main__":
                 break
             photo_url = listing.photo_url
             message = listing.generate_message()
-            if photo_url:
-                print("caption", message)
-                BOT.send_photo(
-                    CHANNEL,
-                    photo_url,
-                    caption=message,
-                    parse_mode=telegram.ParseMode.HTML,
-                )
-            else:
-                BOT.send_message(
-                    chat_id=CHANNEL,
-                    text=message, 
-                    parse_mode=telegram.ParseMode.HTML,
-                )
+            # 2 send attempts per listing
+            for _ in range(2):
+                try:
+                    if photo_url:
+                        BOT.send_photo(
+                            CHANNEL,
+                            photo_url,
+                            caption=message,
+                            parse_mode=telegram.ParseMode.HTML,
+                        )
+                        break
+                    else:
+                        BOT.send_message(
+                            chat_id=CHANNEL,
+                            text=message, 
+                            parse_mode=telegram.ParseMode.HTML,
+                        )
+                        break
+                except (TimedOut, NetworkError):
+                    # retry until we've successfully imgs or we run out of attempts   
+                    pass
+                except TelegramError as e:
+                    # log the breaking error and move on to the next listing
+                    logging.error(e)
+                    break
