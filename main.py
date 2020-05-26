8b9d1fd65fdf748b365c628bd70b681707f019cd
Renzo Gabriel Bongocan <renzo@kalibrr.com>
2020-05-26 23:28:31 +0800

    Autoformat and delint

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from bs4 import BeautifulSoup
 from datetime import timedelta
-from requests.exceptions import RequestException, Timeout
-from pprint import PrettyPrinter
 from telegram.error import TelegramError, TimedOut, NetworkError
-from typing import Any, Dict, List
+from typing import List
 
-import logging
 import random
-import re
 import redis
-import requests
 import telegram
 import yaml
 
@@ -20,19 +14,27 @@ BOT = telegram.Bot(config["telegram"]["bot"]["token"])
 CHANNEL = config["telegram"]["channel"]
 BRANDS = config["listing"]["brands"]
 RECENCY = config["listing"]["recency"]  # maximum age (in days) of listing allowed
-TTL = 60 * 60 * 24 * config["listing"]["ttl"]  # expiry (in days) of listing redis entry (interpreted as "message already sent")
+TTL = (
+    60 * 60 * 24 * config["listing"]["ttl"]
+)  # expiry (in days) of listing redis entry (interpreted as "message already sent")
 
 TITLE_LIMIT = config["message"]["limit"]["title"]
 DESCRIPTION_LIMIT = config["message"]["limit"]["title"]
 
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host="redis", port=6379)
 
 
 def generate_message(listing: CarousellListing) -> str:
     # assume the listing is valid
-    title = t if len((t := listing.title)) < TITLE_LIMIT else f"{t[:TITLE_LIMIT].strip()}..."
-    description = d if len((d := listing.description)) else f"{d[:DESCRIPTION_LIMIT].strip()}..."
-    message = f"<a href=\"{listing.url}\">{title.title()}</a>"
+    title = (
+        t
+        if len((t := listing.title)) < TITLE_LIMIT
+        else f"{t[:TITLE_LIMIT].strip()}..."
+    )
+    description = (
+        d if len((d := listing.description)) else f"{d[:DESCRIPTION_LIMIT].strip()}..."
+    )
+    message = f'<a href="{listing.url}">{title.title()}</a>'
     message += f"\n{listing.price}"
     if listing.others:
         others = "\n".join(listing.others)
@@ -44,11 +46,15 @@ def generate_message(listing: CarousellListing) -> str:
 def run_script(dry_run=True):
     # TODO: proper logging
     for brand in random.sample(BRANDS, 3):
-        search = CarousellSearch(brand).filter(collections=CarousellCategories.ALL_MENS_FASHION)
+        search = CarousellSearch(brand).filter(
+            collections=CarousellCategories.ALL_MENS_FASHION
+        )
         listings: List[CarousellListing] = search.execute()
         unfiltered_len = len(listings)
         listings = CarousellListing.filter_recent(listings, timedelta(days=RECENCY))
-        print(f"Got {unfiltered_len} listings under {brand}; {len(listings)} are recent")
+        print(
+            f"Got {unfiltered_len} listings under {brand}; {len(listings)} are recent"
+        )
         for listing in listings:
             if cache.exists(listing.id):
                 print(f"Already sent: {listing.id} - {listing.title}")
@@ -59,7 +65,7 @@ def run_script(dry_run=True):
             message = generate_message(listing)
             try:
                 tg_message = None
-                if (url := listing.photo_url):
+                if url := listing.photo_url:
                     tg_message: telegram.Message = BOT.send_photo(
                         CHANNEL,
                         url,
@@ -69,7 +75,7 @@ def run_script(dry_run=True):
                 else:
                     tg_message: telegram.Message = BOT.send_message(
                         chat_id=CHANNEL,
-                        text=message, 
+                        text=message,
                         parse_mode=telegram.ParseMode.HTML,
                     )
                 if tg_message:
--- search.py
+++ search.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from datetime import datetime, timedelta
 from enum import Enum
-from pprint import PrettyPrinter
 from time import time
 from typing import Any, Dict, List
 
@@ -15,8 +14,8 @@ CAROUSELL_API = "api-service/filter/search/3.3/products/"
 
 
 class CarousellCategories(Enum):
-   ALL_MENS_FASHION = "3"
-   WOMENS_BAG_AND_WALLETS = "844"
+    ALL_MENS_FASHION = "3"
+    WOMENS_BAG_AND_WALLETS = "844"
 
 
 class CarousellListing:
@@ -29,7 +28,7 @@ class CarousellListing:
         price=None,
         description=None,
         others=[],
-        photo_url=None
+        photo_url=None,
     ) -> None:
         self.id = id
         self.created_at = created_at
@@ -39,11 +38,9 @@ class CarousellListing:
         self.others = others
         self.photo_url = photo_url
 
-    
     @property
     def url(self) -> str:
-        return f"{CAROUSELL_HOST}/p/{self.id}" 
-
+        return f"{CAROUSELL_HOST}/p/{self.id}"
 
     def json(self) -> Dict[str, Any]:
         return {
@@ -57,33 +54,28 @@ class CarousellListing:
             "photo_url": self.photo_url,
         }
 
-
     def __repr__(self) -> str:
         return str(self.json())
 
-    
     @staticmethod
-    def filter_recent(listings: List[CarousellListing], age_lte: timedelta) -> List[CarousellListing]:
+    def filter_recent(
+        listings: List[CarousellListing], age_lte: timedelta
+    ) -> List[CarousellListing]:
         """
         Filter listings in-python with max age allowed specified by timedelta
         """
         return [
-            l for l in listings if (datetime.now() - l.created_at <= age_lte) 
+            listing
+            for listing in listings
+            if (datetime.now() - listing.created_at <= age_lte)
         ]
-        
 
 
 class CarousellSearch:
-    def __init__(
-        self,
-        term=None,
-        count=20,
-        filters={}
-    ):
+    def __init__(self, term=None, count=20, filters={}):
         self.term = term
         self.count = count
         self._filters = filters
-
 
     def filter(self, **kwargs) -> CarousellSearch:
         new_filters = copy.deepcopy(self._filters)
@@ -91,22 +83,12 @@ class CarousellSearch:
             if k not in new_filters:
                 new_filters[k] = []
             new_filters[k].append(v.value if isinstance(v, Enum) else v)
-        return self._copy_with(
-            term=self.term,
-            count=self.count,
-            filters=new_filters
-        )
-
+        return self._copy_with(term=self.term, count=self.count, filters=new_filters)
 
     def _copy_with(self, **kwargs) -> CarousellSearch:
-        new_kwargs = dict(
-            term=self.term,
-            count=self.count,
-            filters=self._filters,
-        )
+        new_kwargs = dict(term=self.term, count=self.count, filters=self._filters,)
         new_kwargs.update(kwargs)
         return CarousellSearch(**new_kwargs)
-
 
     def json(self):
         params = {
@@ -118,31 +100,17 @@ class CarousellSearch:
             "isFreeItems": False,
             "locale": "en",
             # TODO: parametrize? Hardcoded is to sort by most recent
-            "prefill": {
-                "prefill_sort_by": "time_created,descending"
-            },
-            "sortParam": {
-                "fieldName": "time_created",
-                "ascending": {
-                    "value": False
-                }
-            }
+            "prefill": {"prefill_sort_by": "time_created,descending"},
+            "sortParam": {"fieldName": "time_created", "ascending": {"value": False}},
         }
         for k, v in self._filters.items():
             # this is how the carousell API expects it
-            params["filters"].append({
-                "fieldName": k,
-                "idsOrKeywords": {
-                    "value": v
-                }
-            })
+            params["filters"].append({"fieldName": k, "idsOrKeywords": {"value": v}})
 
         return params
 
-    
     def __repr__(self) -> str:
         return str(self.json())
-
 
     def execute(self) -> List[CarousellListing]:
         # turn into a generator instead?
@@ -150,8 +118,14 @@ class CarousellSearch:
         resp = requests.post(f"{CAROUSELL_HOST}/{CAROUSELL_API}", json=self.json())
         results = resp.json()["data"].get("results", [])
         for lc in [r["listingCard"] for r in results]:
-            ts_component = next((c for c in lc["aboveFold"] if c["component"] == "time_created"), {})
-            ts = ts_component.get("timestampContent", {}).get("seconds", {}).get("low", int(time()))
+            ts_component = next(
+                (c for c in lc["aboveFold"] if c["component"] == "time_created"), {}
+            )
+            ts = (
+                ts_component.get("timestampContent", {})
+                .get("seconds", {})
+                .get("low", int(time()))
+            )
             kwargs = {
                 "id": lc["id"],
                 "created_at": datetime.fromtimestamp(ts),
@@ -167,13 +141,13 @@ class CarousellSearch:
                         key = "description"
                     else:
                         key = "others"
-                
+
                 content = component["stringContent"]
                 if key == "others":
                     if content:
                         kwargs[key].append(content)
                 else:
-                    kwargs[key] = content  
+                    kwargs[key] = content
             if lc["media"]:
                 kwargs["photo_url"] = lc["media"][0].get("photoItem", {}).get("url")
             listings.append(CarousellListing(**kwargs))
